e8bb79e4953ef605042931830986206a316fdd47
Bartosz Pranczke <pranczke@gmail.com>
2011-10-01 21:41:43 +0200

    Crawler returns all needed information

--- crawler/crawler.rb
+++ crawler/crawler.rb
@@ -9,15 +9,19 @@ require 'nokogiri'
 domain = ARGV[0]
 email_regex = /[\w+\-.]+@[a-z\d\-.]+\.[a-z]+/i
 arr_mails = Array.new
+out_links = Array.new
 
 Anemone.crawl(domain) do |anemone|
   
   anemone.focus_crawl do |page|
-    page.links.select! do |x|
+    domain_links = page.links.select do |x|
       x.to_s.downcase.include? domain.downcase
     end
+    out_links = out_links + page.links - domain_links
+    domain_links
   end
 
+  anemone.user_agent = "ChgCrawler"
   anemone.on_every_page do |page|
     puts page.url
     mails = page.body.scan(email_regex)
@@ -25,7 +29,9 @@ Anemone.crawl(domain) do |anemone|
       arr_mails.push(mail) unless arr_mails.index(mail) 
     end    
   end
+
 end
 
+puts "Links: #{out_links.uniq!}"
 puts "Emails found in #{domain}"
 puts arr_mails

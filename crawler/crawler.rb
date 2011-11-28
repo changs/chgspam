74eff9e9bf6be08c02e8865d460b5b9cfdc12724
Bartosz Pranczke <pranczke@gmail.com>
2011-11-28 23:49:37 +0100

    Crawl only on 500 links

--- crawler/crawler.rb
+++ crawler/crawler.rb
@@ -56,6 +56,7 @@ Anemone.crawl(domain) do |anemone|
     page.links.select do |x|
       x.to_s.downcase.include? domain.downcase
     end
+    page.links.slice(0..500)
   end
   
   anemone.user_agent = "ChgCrawler"
@@ -97,10 +98,10 @@ send_to_server(server_url + '/email', { 'emails' => mails[x...mails.count], 'dom
 x = 0
 alinks = links.to_a
 while x < alinks.count-10
-  send_to_server(server_url + '/link',  { 'url' => alinks[x...alinks.count ])
+  send_to_server(server_url + '/link',  { 'url' => alinks[x...alinks.count ]})
   x+=10
 end
-send_to_server(server_url + '/link',  { 'url' => alinks[x...alinks.count])
+send_to_server(server_url + '/link',  { 'url' => alinks[x...alinks.count]})
 
 puts "Sleep for a while" 
 sleep 5
